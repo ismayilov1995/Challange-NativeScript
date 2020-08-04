d1faa39c08bb4aa2ed4109505fc5985a106956be
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-04 17:43:20 +0400

    Created First Component

---
+++ src/app/challanges/current-challange/current-challange.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "ns-current-challange",
+    templateUrl: "current-challange.component.html",
+    styleUrls: ["current-challange.component.css"],
+})
+export class CurrentChallangeComponent implements OnInit {
+    challangeDescription = "";
+    currentChallange: string;
+
+    constructor() {}
+
+    ngOnInit() {}
+
+    onSetChallange(): void {
+        this.currentChallange = this.challangeDescription
+    }
+}
