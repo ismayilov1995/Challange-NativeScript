1558932f25dffdaae969a540f0df62b1fc98e9b5
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-04 21:11:14 +0400

    Grid Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from "./app.component";
 import { CurrentChallangeComponent } from "./challanges/current-challange/current-challange.component";
 import { StackComponent } from './layouts/stack/stack.component';
 import { FlexboxComponent } from './layouts/flexbox/flexbox.component';
+import { GridComponent } from './layouts/grid/grid.component';
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
@@ -12,7 +13,7 @@ import { FlexboxComponent } from './layouts/flexbox/flexbox.component';
 @NgModule({
     bootstrap: [AppComponent],
     imports: [NativeScriptModule, NativeScriptFormsModule],
-    declarations: [AppComponent, CurrentChallangeComponent, StackComponent, FlexboxComponent],
+    declarations: [AppComponent, CurrentChallangeComponent, StackComponent, FlexboxComponent, GridComponent],
     providers: [],
     schemas: [NO_ERRORS_SCHEMA],
 })
