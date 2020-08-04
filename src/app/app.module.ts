bb236f207586b0a85d3f893205c8479275a3c5ac
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-04 17:41:56 +0400

    Create first compponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,17 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { NativeScriptModule } from "@nativescript/angular";
-import { NativeScriptHttpClientModule } from "@nativescript/angular/http-client/http-client.module";
+import { NativeScriptModule, NativeScriptFormsModule } from "@nativescript/angular";
 
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
-
-// Uncomment and add to NgModule imports if you need to use two-way binding
-// import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { CurrentChallangeComponent } from "./challanges/current-challange/current-challange.component";
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
 
 @NgModule({
     bootstrap: [AppComponent],
-    imports: [
-        NativeScriptModule,
-        AppRoutingModule,
-        NativeScriptHttpClientModule,
-        HttpClientModule
-    ],
-    declarations: [AppComponent, ItemsComponent, ItemDetailComponent],
+    imports: [NativeScriptModule, NativeScriptFormsModule],
+    declarations: [AppComponent, CurrentChallangeComponent],
     providers: [],
     schemas: [NO_ERRORS_SCHEMA],
 })
-/*
-Pass your application module to the bootstrapModule function located in main.ts to start your app
-*/
 export class AppModule {}
