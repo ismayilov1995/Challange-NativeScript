cb6fd538699d7754599032a0a935013f21763ec1
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-07 23:59:53 +0400

    Change theme

--- src/app/challanges/challange-tabs/challange-tabs.component.ts
+++ src/app/challanges/challange-tabs/challange-tabs.component.ts
@@ -6,7 +6,7 @@ import { Page } from "tns-core-modules/ui/page";
 @Component({
     selector: "ns-challange-tabs",
     templateUrl: "./challange-tabs.component.html",
-    styleUrls: ["./challange-tabs.component.css"],
+    styleUrls: ["./challange-tabs.component.css", "./challange-tabs.component.common.css"],
 })
 export class ChallangeTabsComponent implements OnInit {
     constructor(
