9c611316574f31610606274c0d489f314220e7e0
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-08 23:22:08 +0400

    Add create challange method to service

--- src/app/challanges/challenge.service.ts
+++ src/app/challanges/challenge.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { Challenge } from "./challenge.model";
+import { Observable } from "tns-core-modules/ui/page";
 
 @Injectable({
     providedIn: "root",
@@ -9,4 +10,19 @@ export class ChallengeService {
     private _currentChallenge = new BehaviorSubject<Challenge>(null);
 
     constructor() {}
+
+    get currentChallenge() {
+        return this._currentChallenge.asObservable();
+    }
+
+    createNewChallange(title: string, description: string) {
+        const challange = new Challenge(
+            title,
+            description,
+            new Date().getFullYear(),
+            new Date().getMonth()
+        );
+        // Save it to server
+        this._currentChallenge.next(challange);
+    }
 }
