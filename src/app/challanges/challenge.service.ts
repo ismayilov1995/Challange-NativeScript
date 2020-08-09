15dbef268ba353cf418e6e95132283618fe60d11
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-09 17:28:15 +0400

    Update day status

--- src/app/challanges/challenge.service.ts
+++ src/app/challanges/challenge.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
 import { Challenge } from "./challenge.model";
+import { DayStatus } from "./day.model";
+import { last, take } from "rxjs/operators";
 
 @Injectable({ providedIn: "root" })
 export class ChallengeService {
@@ -21,5 +23,18 @@ export class ChallengeService {
         );
         // Save it to server
         this._currentChallenge.next(challange);
+    }
+
+    updateDayStatus(dayInMonth: number, status: DayStatus) {
+        this._currentChallenge.pipe(take(1)).subscribe((challenge) => {
+            if (!challenge || challenge.days.length < dayInMonth) {
+                return;
+            }
+            const dayIndex = challenge.days.findIndex(
+                (d) => d.dayInMonth === dayInMonth
+            );
+            challenge.days[dayIndex].status = status;
+            this._currentChallenge.next(challenge);
+        });
     }
 }
