04185f21db35c489a2d1913f5ad1dcff674d23cd
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-10 15:55:45 +0400

    Handle response error

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { tap } from "rxjs/operators";
+import { tap, catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
+import { alert } from "tns-core-modules/ui/dialogs";
 
 const FIREBASE_API = "AIzaSyDpzC8ApalwDibZv4BOSgmjSJg0bvi16ag";
 
@@ -14,18 +16,49 @@ export class AuthService {
     constructor(private http: HttpClient) {}
 
     login(email: string, password: string) {
-        return this.http.post(this.loginUrl, {
-            email,
-            password,
-            returnSecureToken: "true",
-        });
+        return this.http
+            .post(this.loginUrl, {
+                email,
+                password,
+                returnSecureToken: "true",
+            })
+            .pipe(
+                catchError((errorRes) => {
+                    this.handleError(errorRes.error.error.message);
+                    return throwError(errorRes);
+                })
+            );
     }
 
     signup(email: string, password: string) {
-        return this.http.post(this.url, {
-            email,
-            password,
-            returnSecureToken: "true",
-        });
+        return this.http
+            .post(this.url, {
+                email,
+                password,
+                returnSecureToken: "true",
+            })
+            .pipe(
+                catchError((errorRes) => {
+                    this.handleError(errorRes.error.error.message);
+                    return throwError(errorRes);
+                })
+            );
+    }
+
+    private handleError(errorMessage: string) {
+        switch (errorMessage) {
+            case "EMAIL_EXISTS":
+                alert("Email used already");
+                break;
+            case "EMAIL_NOT_FOUND":
+                alert("Email not found");
+                break;
+            case "INVALID_PASSWORD":
+                alert("Password incorrect");
+                break;
+            default:
+                alert("We got an error");
+                break;
+        }
     }
 }
